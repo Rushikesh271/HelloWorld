de303dbfb91c92b78cfdf98426512132044746c1
Rushikesh271 <rushikesh.joshi271@gmail.com>
2023-04-07 13:20:17 +0530

    HelloWorld1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
-public class HelloWorld{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World");
-	}
+public class HelloWorld {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    System.out.println(sum); // Print the sum of x + y
+  }
 }
